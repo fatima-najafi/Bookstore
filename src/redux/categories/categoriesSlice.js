1e7e454bef3a0ea1f50e2c63c9c0ef8f6fa06bbd
Fatima Najafi <najafifatima762@gmail.com>
2023-08-21 23:52:32 +0430

    Fixed the linters

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -9,10 +9,9 @@ const categoriesSlice = createSlice({
   initialState,
   reducers: {
     checkStatus: (state, action) => {
-      state.categories =
-        action.payload === 'Under construction'
-          ? ['Under construction']
-          : state.categories;
+      state.categories = action.payload === 'Under construction'
+        ? ['Under construction']
+        : state.categories;
     },
   },
 });
