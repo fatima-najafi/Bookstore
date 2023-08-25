a95ebc749dc193a3a705f6e8b96e501d2eaa1e44
Fatima Najafi <najafifatima762@gmail.com>
2023-08-25 16:23:24 +0430

    Update the file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,35 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import Book from './Book';
-// import { fetchBooks, removeBook } from '../redux/books/booksSlice';
-
-// const BookList = () => {
-//   const books = useSelector((state) => state.books.books);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchBooks());
-//   }, [dispatch]);
-
-//   const handleDelete = (id) => {
-//     dispatch(removeBook(id));
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//         {books.map((book) => (
-//           <li className="book-list" key={book.id}>
-//             <Book book={book} onDelete={() => handleDelete(book.id)} />
-//           </li>
-//         ))}
-//       </ul>
-//       <hr className="Line" />
-//     </div>
-//   );
-// };
-
-// export default BookList;
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
