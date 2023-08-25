6e07d456edfb93621cb95e7b99181a571d30a5ea
Fatima Najafi <najafifatima762@gmail.com>
2023-08-25 16:23:12 +0430

    Update the file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,21 +4,15 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <nav className="nav-bar">
     <div className="nav-brand">
-      <Link to="/bookstore-cms/">
-        <h1>Bookstore CMS</h1>
-      </Link>
+      <Link to="/bookstore-cms/"><h1>Bookstore CMS</h1></Link>
     </div>
     <ul className="nav-links">
       <li>
-        <Link className="nav-link" to="/bookstore-cms/">
-          BOOKS
-        </Link>
+        <Link className="nav-link" to="/bookstore-cms/">BOOKS</Link>
       </li>
       <li>|</li>
       <li>
-        <Link className="nav-link" to="/bookstore-cms/categories">
-          CATEGORIES
-        </Link>
+        <Link className="nav-link" to="/bookstore-cms/categories">CATEGORIES</Link>
       </li>
     </ul>
   </nav>
