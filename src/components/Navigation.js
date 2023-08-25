28ec51b02631670130021e61ca4b73e9494eb398
Fatima Najafi <najafifatima762@gmail.com>
2023-08-25 20:46:51 +0430

    Add logo for bookstore

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styling/Navigation.css';
 
 const Navigation = () => (
   <nav className="nav-bar">
-    <div className="nav-brand">
-      <Link to="/bookstore-cms/"><h1>Bookstore CMS</h1></Link>
+    <div className="nav-item">
+      <Link to="/bookstore-cms/">
+        <h1>Bookstore CMS</h1>
+      </Link>
     </div>
     <ul className="nav-links">
       <li>
-        <Link className="nav-link" to="/bookstore-cms/">BOOKS</Link>
+        <Link className="nav-link" to="/bookstore-cms/">
+          BOOKS
+        </Link>
       </li>
       <li>|</li>
       <li>
-        <Link className="nav-link" to="/bookstore-cms/categories">CATEGORIES</Link>
+        <Link className="nav-link" to="/bookstore-cms/categories">
+          CATEGORIES
+        </Link>
       </li>
     </ul>
+    <div className="icon">
+      <img
+        src="https://img.icons8.com/ios-filled/0290ff/30/user.png"
+        alt="user"
+      />
+    </div>
   </nav>
 );
 
